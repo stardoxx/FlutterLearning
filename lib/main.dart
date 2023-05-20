e904ce68cb19e827dc85a70008a8253354cacb21
Ashutosh Singh Yadav <ashutoshsinghyadav@Ashutoshs-MacBook-Air.local>
2023-05-20 17:26:04 +0530

    adding images

--- lib/main.dart
+++ lib/main.dart
@@ -17,13 +17,7 @@ class Home extends StatelessWidget {
           backgroundColor: Colors.lime
       ),
       body: Center(
-        child: Text("Hey hi There !!",
-          style: TextStyle(
-            fontSize: 20.1,
-            fontWeight: FontWeight.bold,
-            letterSpacing: 1.5,
-            color: Colors.blueGrey,
-          ),),
+        child: Image.network('https://images.unsplash.com/photo-1684183271777-d739f1540c9e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDh8eEh4WVRNSExnT2N8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=60'),
       ),
 
       floatingActionButton: FloatingActionButton(
