ba25bb14712c8afca302b1c8b520bd757567cb62
Ashutosh Singh Yadav <ashutoshsinghyadav@Ashutoshs-MacBook-Air.local>
2023-05-20 16:58:17 +0530

    stateless widget and hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text("My first App"),
-      centerTitle: true,
-      backgroundColor: Colors.lime
-    ),
-    body: Center(
-    child: Text("Hey hi There !!",
-    style: TextStyle(
-      fontSize: 20.1,
-      fontWeight: FontWeight.bold,
-      letterSpacing: 1.5,
-      color: Colors.blueGrey,
-    ),),
-    ),
-
-    floatingActionButton: FloatingActionButton(
-      child: Text('Click'),
-      onPressed: (){},
-      backgroundColor: Colors.redAccent,
-    ),
-
-  )
+  home: Home()
 
 ));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("My first App"),
+          centerTitle: true,
+          backgroundColor: Colors.lime
+      ),
+      body: Center(
+        child: Text("Hey hi There !!",
+          style: TextStyle(
+            fontSize: 20.1,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.5,
+            color: Colors.blueGrey,
+          ),),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        child: Text('Click'),
+        onPressed: (){},
+        backgroundColor: Colors.redAccent,
+      ),
+
+    );
+  }
+}
