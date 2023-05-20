7f02fb67922791cbcd125390f39b1551b46e19bf
Ashutosh Singh Yadav <ashutoshsinghyadav@Ashutoshs-MacBook-Air.local>
2023-05-20 15:20:48 +0530

    colors and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,22 @@ void main() => runApp(MaterialApp(
     appBar: AppBar(
       title: Text("My first App"),
       centerTitle: true,
+      backgroundColor: Colors.lime
     ),
     body: Center(
-    child: Text("Hey hi There !!"),
+    child: Text("Hey hi There !!",
+    style: TextStyle(
+      fontSize: 20.1,
+      fontWeight: FontWeight.bold,
+      letterSpacing: 1.5,
+      color: Colors.blueGrey,
+    ),),
     ),
 
     floatingActionButton: FloatingActionButton(
       child: Text('Click'),
-      onPressed: null
+      onPressed: (){},
+      backgroundColor: Colors.redAccent,
     ),
 
   )
