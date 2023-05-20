7a7f1a52bcb392c8eb0a31c65616815b298c1ea1
Ashutosh Singh Yadav <ashutoshsinghyadav@Ashutoshs-MacBook-Air.local>
2023-05-20 06:19:20 +0530

    scaffold & AppBar

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text("My first App"),
+      centerTitle: true,
+    ),
+    body: Center(
+    child: Text("Hey hi There !!"),
+    ),
+
+    floatingActionButton: FloatingActionButton(
+      child: Text('Click'),
+      onPressed: null,
+    ),
+
+  )
+
+));
